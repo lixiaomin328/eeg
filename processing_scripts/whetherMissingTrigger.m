fb4add8c4e2b6ba69b676cd78d65108a59acbf6d
hottup <lixiaominxiaomin@gmail.com>
2019-08-26 19:30:59 -0700

    processing

---
+++ processing_scripts/whetherMissingTrigger.m
@@ -0,0 +1,11 @@
+function [nFromEEGfile,nFromBehaviorData] = whetherMissingTrigger(subId)
+LoadingDir = '../EEG_data/';
+load('startingTriggerTrial');
+startingTrial = startingTrigerTrial(2,subId-9)+1;
+filename = [num2str(subId),'a.set'];
+run '../eeglab.m'
+EEG = pop_loadset('filename',filename,'filepath',LoadingDir);
+
+nFromEEGfile = sum([EEG.event.type]==65529|[EEG.event.type]==65530|[EEG.event.type]==65532|[EEG.event.type]==1|[EEG.event.type]==2|[EEG.event.type]==4);
+[p2DecisionStart,p2DecisionMade,recoveredTrigger] = triggerFindP2(subId,startingTrial);
+nFromBehaviorData = length(recoveredTrigger);
