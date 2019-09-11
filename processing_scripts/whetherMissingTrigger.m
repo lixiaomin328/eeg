8775bd47d9ea31377c6338b4790ddaa3724331d4
Xiaomin Li <xiaominli@xiaomins-mbp.lan>
2019-09-11 00:09:40 -0700

    confilict

--- processing_scripts/whetherMissingTrigger.m
+++ processing_scripts/whetherMissingTrigger.m
@@ -1,16 +1,18 @@
 function [nFromEEGfile,nFromBehaviorData] = whetherMissingTrigger(subId)
-LoadingDir = '/Volumes/colin/EEG_data/';
 if subId<15
 load('startingTriggerTrial');
 startingTrial = startingTrigerTrial(2,subId-9)+1;
 else 
 startingTrial =1;
 end
-filename = [num2str(subId),'a.set'];
+LoadingDir = '/Volumes/colin/cutEEGdata/';
+load('startingTriggerTrial');
+filename = [num2str(subId),'b.set'];
+>>>>>>> Stashed changes
 run '../eeglab.m'
 EEG = pop_loadset('filename',filename,'filepath',LoadingDir);
 
 %nFromEEGfile = sum([EEG.event.type]==65529|[EEG.event.type]==65530|[EEG.event.type]==65532|[EEG.event.type]==1|[EEG.event.type]==2|[EEG.event.type]==4);
-[p2DecisionStart,p2DecisionMade,recoveredTrigger] = triggerFindP2(subId,startingTrial);
+[~,~,recoveredTrigger] = triggerFindP2(subId,startingTrial);
 nFromBehaviorData = length(recoveredTrigger);
 nFromEEGfile = length([EEG.event.type]);
